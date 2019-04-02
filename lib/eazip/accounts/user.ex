71422fbaaca3c3e790fe2c7d33c113b96973d55b
sbaudray <sebastien.baudray@hetic.net>
2019-04-02 11:46:50 +0200

    add credential repo

--- lib/eazip/accounts/user.ex
+++ lib/eazip/accounts/user.ex
@@ -1,10 +1,12 @@
 defmodule Eazip.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Eazip.Accounts.Credential
 
   schema "users" do
     field :name, :string
     field :username, :string
+    has_one :credential, Credential
 
     timestamps()
   end
