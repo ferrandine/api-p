2e800b6338651d06193775cd7ca44922a4072ed6
sbaudray <sebastien.baudray@hetic.net>
2019-05-15 21:33:03 +0200

    add alteration labels

--- lib/eazip/alterations/alteration.ex
+++ lib/eazip/alterations/alteration.ex
@@ -5,6 +5,7 @@ defmodule Eazip.Alterations.Alteration do
 
   schema "alterations" do
     field :description, :string
+    field :label, :string
     field :image, :string
     field :type, :string
     belongs_to :alteration_category, AlterationCategory, foreign_key: :category_id
@@ -15,8 +16,8 @@ defmodule Eazip.Alterations.Alteration do
   @doc false
   def changeset(alteration, attrs) do
     alteration
-    |> cast(attrs, [:type, :description, :image])
-    |> validate_required([:type, :description, :image])
+    |> cast(attrs, [:type, :description, :image, :label])
+    |> validate_required([:type, :description, :image, :label])
     |> unique_constraint(:type, name: :alteration_category_id_type_index)
   end
 end
